6b72efdf69906f9285804ec824eef360b92318dc
dariakunitsyna <litaeminoppa@gmail.com>
2026-04-27 04:39:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes
 import random
-import os
 
 def main_menu():
     return InlineKeyboardMarkup([
@@ -10,8 +9,7 @@ def main_menu():
     ])
 
 
-TOKEN = os.getenv("TOKEN")
-
+TOKEN = "8700188117:AAHme6Wm5xrosrU_TFQyBciI72V8HRgDpg0"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
@@ -65,7 +63,101 @@ def back_menu():
         [InlineKeyboardButton("🔙 Назад", callback_data="back")]
     ])
 
+REMNAWAVE_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IklhbVN1cGVyQWRtaW4iLCJ1dWlkIjoiNTI3OWQ0YzItMDAyZC00NjM5LWFhYzEtNWI1NzQ4MWJhMGZiIiwicm9sZSI6IkFETUlOIiwiaWF0IjoxNzc3MjQ4Njc3LCJleHAiOjE3NzcyOTE4Nzd9.B1G_hWyCIzanrKItl4ZetgCqzY5bFPsaj5j2uhKTKkU"
 
+import requests
+from datetime import datetime, timedelta
+
+REMNAWAVE_URL = "https://admin.akajhwbsn.ru"
+
+def get_user(username):
+    url = f"{REMNAWAVE_URL}/api/users?search={username}"
+
+    headers = {
+        "Authorization": f"Bearer {REMNAWAVE_TOKEN}"
+    }
+
+    r = requests.get(url, headers=headers)
+
+    if r.status_code == 200:
+        data = r.json()
+        if data.get("response"):
+            return data["response"][0]  # берём первого
+    return None
+
+def create_user(username, days):
+    url = f"{REMNAWAVE_URL}/api/users"
+
+    headers = {
+        "Authorization": f"Bearer {REMNAWAVE_TOKEN}",
+        "Content-Type": "application/json"
+    }
+
+    expire = (datetime.utcnow() + timedelta(days=days)).isoformat() + "Z"
+
+    payload = {
+        "username": username,
+        "status": "ACTIVE",
+        "trafficLimitBytes": 0,
+        "trafficLimitStrategy": "NO_RESET",
+        "expireAt": expire,
+        "description": f"tg:{username}",
+        "activeInternalSquads": ["fbc3bb64-442f-49bc-a653-0aa12ee7a3cf"]
+    }
+
+    r = requests.post(url, json=payload, headers=headers)
+
+    if r.status_code in (200, 201):
+        return r.json()["response"]
+    else:
+        print(r.text)
+        return None
+
+def update_user(uuid, days):
+    url = f"{REMNAWAVE_URL}/api/users/{uuid}"
+
+    headers = {
+        "Authorization": f"Bearer {REMNAWAVE_TOKEN}",
+        "Content-Type": "application/json"
+    }
+
+    expire = (datetime.utcnow() + timedelta(days=days)).isoformat() + "Z"
+
+    payload = {
+        "expireAt": expire
+    }
+
+    r = requests.patch(url, json=payload, headers=headers)
+
+    return r.status_code == 200
+
+async def handle_buy(query, days):
+    tg_id = str(query.from_user.id)
+
+    user = get_user(tg_id)
+
+    # если пользователь уже есть → продлеваем
+    if user:
+        update_user(user["uuid"], days)
+        sub_url = user["subscriptionUrl"]
+
+        text = "🔄 Подписка продлена!\n\n"
+
+    else:
+        user = create_user(tg_id, days)
+
+        if not user:
+            await query.message.edit_text("❌ Ошибка создания")
+            return
+
+        sub_url = user["subscriptionUrl"]
+        text = "✅ Доступ выдан!\n\n"
+
+    await query.message.edit_text(
+        text + f"🔗 Ваша подписка:\n<code>{sub_url}</code>",
+        parse_mode="HTML",
+        reply_markup=main_menu()
+    )
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
